85ea9d990e0d54adb4087ead032688b6c32a59b5
Gina De La Rosa <g26delarosa@gmail.com>
2016-08-08 23:13:03 -0700

    adding new files

---
+++ SoundsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundsViewController.swift
+//  MyWonders
+//
+//  Created by Gina De La Rosa on 8/8/16.
+//  Copyright © 2016 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class SoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
